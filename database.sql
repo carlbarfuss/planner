d87f0369e9d6aeaf4edcfbc1fabfaae25d43f800
carlbarfuss <carlbarfuss@gmail.com>
2021-01-12 11:44:46 -0600

    database up and running, database.sql updated

--- database.sql
+++ database.sql
@@ -1,10 +1,47 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "password" VARCHAR (1000) NOT NULL,
+    "token" VARCHAR (1000),
+    "email" VARCHAR (1000),
+    "age" INT,
+    "retirement_age" INT, 
+    "target_income" INT,
+    "savings" INT,
+    "saved_monthly" INT,
+    "inflation_rate" DECIMAL(4,2) DEFAULT 2.00,
+    "rate_of_return" DECIMAL(4,2) DEFAULT 10.00
 );
+
+CREATE TABLE "expenses" (
+	"id" SERIAL PRIMARY key,
+	"user_id" INT,
+	"liability_name" VARCHAR(100),
+	"liability_monthly_cost" DECIMAL(8,2),
+	FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+CREATE TABLE "income_streams" (
+	"id" SERIAL PRIMARY key,
+	"user_id" INT,
+	"income_name" VARCHAR(100),
+	"income_monthly_value" DECIMAL(8,2),
+	FOREIGN KEY (user_id) REFERENCES "user"(id)
+);
+
+CREATE TABLE "fed_data" (
+	"id" SERIAL PRIMARY key,
+	"age_min" int,
+	"age_max" int,
+	"average_savings" int,
+	"median_savings" int,
+	"99th_percentile_savings" int
+);
+
+
+INSERT INTO fed_data ("age_min", "age_max", "average_savings", "median_savings", "99th_percentile_savings")
+VALUES (18, 24, 4745.25, 0.00, 101000.00), (25, 29, 9408, 0, 93000), (30, 34, 21731, 2000, 2041000), (35, 39, 48710, 2500, 500000), (40, 44, 101899, 6950, 1097000),
+(45, 49, 148950, 10000, 1794000), (50, 54, 146068, 10000, 1624000), (55, 59, 223493, 15000, 2583000), (60, 64, 221451, 2000, 2520000), (65, 69, 206819, 0, 2683000), 
+(70, 74, 203964, 0, 2739000), (75, 79, 143613, 0, 2300000), (80, 999, 128216, 0, 2720000);
+
+DROP TABLE "income_streams";
--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -8,7 +8,61 @@ import React from 'react';
 const AboutPage = () => (
   <div className="container">
     <div>
-      <p>This about page is for anyone to read!</p>
+      <h1>Retirement Checkup</h1>
+      <h3>What is it?</h3>
+      <p>Retirement Checkup is a self-contained application 
+        designed to provide you, the user, with a snapshot of how you are doing in planning for your retirement. 
+        Retirement is a big, scary, far away goal for most people and the goal of this application is to make it a
+        more approachable topic.</p>
+      <h3>What does it do?</h3>
+      <p>By taking rates of return for the stock market over the past 140 years, as well as average inflation rates, 
+        Retirement Checkup allows a user to see a few things under the lens of passing time:
+      </p>
+      <ol> 
+        <li>How much money will you need to live in X years?</li>
+        <li>What are your current retirement accounts going to be worth if you add Y money to them over the years at retierement?</li>
+        <li>If you collect Social Security, how much will that help?</li>
+        <li>How much will other income streams help? (part time job, rental property, family trust, etc)</li>
+      </ol>
+      <h3>
+        How do I get started?
+      </h3>
+      <p>
+        Go ahead and click the Login/Register button up above and follow the questions to set up your account. You will need 
+        the following to get started:
+      </p>
+        <ol>
+          <li>An idea of how much you need to live per year.</li>
+          <li>A current total in your retirement accounts (401k/Roth Ira/etc).  For this step please only consider money that is in an
+            account that is invested in the stock market.  Multiple accounts should be aggregated into one sum.
+          </li>
+          <li>Rough idea of income streams you will have in retirement. For most people this will be just Social Security. 
+            For others it may include pensions or rental income or annuities.
+          </li>
+        </ol>
+      <h3>
+        I don't have any retirement savings, and I don't even know where to get started?!?
+      </h3>
+      <p>While this app is focussed on people who have some amount of retirement savings, below are resources to get you started 
+        on your journey.  
+      </p>
+        <ul>
+        <li><a href="https://reddit.com/r/PersonalFinance">Personal Finance</a> - This is a community of people on Reddit.com 
+        who are focussed on getting their finances in order.  The Readme documentation addresses almost all of the common "HELP" topics
+        and will set someone focussed on starting to plan their financial future down the right path.</li>
+        </ul>
+      <h3>Ok, I've got all this stuff - how do I turn it up a notch?  I don't wanna work until I'm 65!</h3>
+      <p>
+        For those people who have been saving and are already on track to overshoot their retirement goals, here are some resources to 
+        make that a reality sooner:
+      </p>
+      <ul>
+        <li><a href="https://reddit.com/r/fire">FIRE </a> - Financial Independance Retire Early subreddit on Reddit.com. 
+        This community is dedicated towards strategies to get people out of the day to day grind and enjoying life earlier.</li>
+        <li><a href="https://reddit.com/r/phatfire">PhatFIRE</a> - PHAT Financial Independance Retire Early subreddit on Reddit.com. 
+        This community is dedicated towards those people who have hit it big and want to move away from the corporate world
+         in a bigger manner.</li>
+      </ul>
     </div>
   </div>
 );
